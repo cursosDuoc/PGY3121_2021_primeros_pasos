e5a05b4293cfdc1d9b0e178555bcf7933de5e37e
Daniel Montero <dmonterv@gmail.com>
2021-03-26 10:20:39 -0300

    Segundo commit, agregamos el trabajo de las clases anteriores

---
+++ primero.js
@@ -0,0 +1,10 @@
+// primero.js
+// archivo JS para nuestra primera pagina
+// Solo un script tonto para ver que JS funciona
+
+// script tonto!!!
+/*
+for(var i = 0; i < 5 ; i++){
+    alert( "mensaje " + i);
+}
+*/
